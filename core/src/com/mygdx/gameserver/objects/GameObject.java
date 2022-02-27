e7cd089fa24508324e01edd2f2d4d00eadc6370c
Alan Parik <alpari@ttu.ee>
2022-02-27 18:41:02 +0200

    Game object abstract class added

---
+++ core/src/com/mygdx/gameserver/objects/GameObject.java
@@ -0,0 +1,45 @@
+package com.mygdx.gameserver.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Polygon;
+
+public abstract class GameObject {
+
+    public Polygon polygon;
+    public Sprite sprite;
+
+    /**
+     * Constructor for all objects on the screen.
+     * @param texture object texture.
+     * @param x X-coordinate.
+     * @param y Y-coordinate.
+     * @param width object width.
+     * @param height object height.
+     */
+    public GameObject(Texture texture, float x, float y, float width, float height) {
+
+        // Create a Sprite with given texture
+        sprite = new Sprite(texture);
+        sprite.setSize(width, height); // resize the texture
+        sprite.setOrigin(width / 2f, height / 2f); // set texture center
+        sprite.setPosition(x - width / 2f, y - height / 2f); // set texture position
+
+        // Create a Polygon with given vertices
+        polygon = new Polygon(new float[]{0f, 0f, width, 0f, width, height, 0f, height});
+        polygon.setOrigin(width / 2f, height / 2f); // set polygon center
+        polygon.setPosition(x - width / 2f, y - height / 2f); // set polygon position
+
+    }
+
+    /**
+     * Draw an object on the batch.
+     * @param batch batch.
+     */
+    public void draw(SpriteBatch batch) {
+        sprite.setPosition(polygon.getX(), polygon.getY()); // set Sprite position equal to Polygon position
+        sprite.setRotation(polygon.getRotation()); // set Sprite rotation around the Polygon center
+        sprite.draw(batch); // draw an object on the batch
+    }
+}
