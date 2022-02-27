f6998ebb43cbbcfd8e3e0b1b6b70af577c812b71
Alan Parik <alpari@ttu.ee>
2022-02-27 19:09:43 +0200

    Character class do not extend game object.

---
+++ core/src/com/mygdx/gameserver/objects/Player.java
@@ -0,0 +1,16 @@
+package com.mygdx.gameserver.objects;
+
+public class Player extends GameObject {
+
+    /**
+     * Constructor for all objects on the screen.
+     *
+     * @param x      X-coordinate.
+     * @param y      Y-coordinate.
+     * @param width  object width.
+     * @param height object height.
+     */
+    public Player(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
