9770604e047101d8d0e679d0b1e3882026734787
Alan Parik <alpari@ttu.ee>
2022-02-28 20:33:49 +0200

    Added new 'character' field

--- core/src/com/mygdx/gameserver/objects/Player.java
+++ core/src/com/mygdx/gameserver/objects/Player.java
@@ -2,15 +2,19 @@ package com.mygdx.gameserver.objects;
 
 public class Player extends GameObject {
 
+    private Character character;
+
     /**
      * Constructor for all objects on the screen.
      *
-     * @param x      X-coordinate.
-     * @param y      Y-coordinate.
-     * @param width  object width.
-     * @param height object height.
+     * @param x         X-coordinate.
+     * @param y         Y-coordinate.
+     * @param width     object width.
+     * @param height    object height.
+     * @param character character (hero) player has chosen to play with
      */
-    public Player(float x, float y, float width, float height) {
+    public Player(float x, float y, float width, float height, Character character) {
         super(x, y, width, height);
+        this.character = character;
     }
 }
