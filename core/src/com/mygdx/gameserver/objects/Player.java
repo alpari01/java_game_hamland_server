0f9109ff1ad53fafe4680bf3a4b62ee21a52959a
alpari <alpari@ttu.ee>
2022-03-01 11:44:49 +0200

    Player getters/setters added

--- core/src/com/mygdx/gameserver/objects/Player.java
+++ core/src/com/mygdx/gameserver/objects/Player.java
@@ -3,6 +3,10 @@ package com.mygdx.gameserver.objects;
 public class Player extends GameObject {
 
     private Character character;
+    private float x;
+    private float y;
+    private float width;
+    private float height;
 
     /**
      * Constructor for all objects on the screen.
@@ -15,6 +19,43 @@ public class Player extends GameObject {
      */
     public Player(float x, float y, float width, float height, Character character) {
         super(x, y, width, height);
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.character = character;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float newX) {
+        x = newX;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float newY) {
+        y = newY;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character newCharacter) {
+        character = newCharacter;
     }
 }
