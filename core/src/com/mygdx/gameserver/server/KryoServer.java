9ff386e3daa6914fb565e8ebadb355c1c4e2f8a0
Alan Parik <alpari@ttu.ee>
2022-03-07 18:06:37 +0200

    Packet check nickname unique implemented

--- core/src/com/mygdx/gameserver/server/KryoServer.java
+++ core/src/com/mygdx/gameserver/server/KryoServer.java
@@ -42,13 +42,53 @@ public class KryoServer extends Listener {
     // Run this method when a client connects.
     public void connected(Connection c) {
         System.out.println("Received a connection from " + c.getRemoteAddressTCP().getHostString());
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         PacketMessage packetMessage = new PacketMessage();
         packetMessage.message = "Privet kak dela";
         c.sendTCP(packetMessage);
     }
 
+    // Run this method when server receives any packet from ANY client.
+    public void received(Connection c, Object p) {
+
+        // Packet check nickname unique.
+        if (p instanceof PacketCheckPlayerNicknameUnique) {
+            PacketCheckPlayerNicknameUnique packet = (PacketCheckPlayerNicknameUnique) p;
+
+            // Check if this nickname is not already taken by other player.
+            if (!connectedPlayers.containsKey(packet.playerNickname)) {
+                addPlayer(packet.playerNickname);
+                System.out.println("Client nickname is " + packet.playerNickname);
+                System.out.println(connectedPlayers);
+            }
+        }
+    }
+
     // Run this method when a client disconnects.
     public void disconnected(Connection c) {
         System.out.println("A client with IP " + c.getRemoteAddressTCP() + " has disconnected.");
+    }
+
+    /**
+     * Create and add new player object to connected players list.
+     *
+     * @param playerNickname nickname of the player
+     */
+    public void addPlayer(String playerNickname) {
+        Player newPlayer = new Player(0, 0, 100, 100, null);
+        connectedPlayers.put(playerNickname, newPlayer);
+    }
+
+    /**
+     * Remove player from connected players list.
+     *
+     * @param playerNickname nickname of the player to remove.
+     */
+    public void removePlayer(String playerNickname) {
+        connectedPlayers.remove(playerNickname);
     }
 }
